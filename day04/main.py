3299bba8f19cd3bf7e5228e126a53a00030cbaa5
Breno Caldeira Nascimento <breno.cn@hotmail.com>
2025-12-04 16:18:12 -0300

    day 4 part 2

--- day04/main.py
+++ day04/main.py
@@ -2,14 +2,14 @@ import os
 import sys
 from typing import List
 
-def read_input() -> List[str]:
+def read_input() -> List[List[str]]:
     with open(os.path.join(sys.path[0], 'input.txt')) as file:
-        return list(map(lambda x: x.strip(), file.readlines()))
+        return list(map(lambda x: list(x.strip()), file.readlines()))
 
-def is_valid_position(board: List[str], i: int, j: int) -> bool:
+def is_valid_position(board: List[List[str]], i: int, j: int) -> bool:
     return i >= 0 and i < len(board) and j >= 0 and j < len(board[0])
 
-def is_position_reachable(board: List[str], i: int, j: int) -> bool:
+def is_position_reachable(board: List[List[str]], i: int, j: int) -> bool:
     total = 0
 
     for new_i in range(i - 1, i + 2):
@@ -25,7 +25,7 @@ def is_position_reachable(board: List[str], i: int, j: int) -> bool:
 
     return total < 4
             
-def part1(board: List[str]) -> int:
+def part1(board: List[List[str]]) -> int:
     result = 0
 
     for i in range(len(board)):
@@ -38,7 +38,30 @@ def part1(board: List[str]) -> int:
 
     return result
 
+def part2(board: list[List[str]]) -> int:
+    result = 0
+
+    while True:
+        removedPaper = False
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == '.':
+                    continue
+
+                if is_position_reachable(board, i, j):
+                    board[i][j] = '.'
+                    result += 1
+                    removedPaper = True
+        
+        if not removedPaper:
+            break
+
+    return result
+
     
 board = read_input()
 result_part_1 = part1(board)
 print(result_part_1)
+
+result_part_2 = part2(board)
+print(result_part_2)
